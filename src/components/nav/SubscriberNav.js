6bb5578c1541b38e8d4baad955c10e827b5f117e
Nainji Hora <nainji.hora@qdegrees.com>
2023-11-03 19:53:05 +0530

    s

--- src/components/nav/SubscriberNav.js
+++ src/components/nav/SubscriberNav.js
@@ -68,10 +68,10 @@ const SubscriberNav = () => {
           variant="permanent"
           open={!collapsed}
           sx={{
-            width: collapsed ? 73 : "100%",
+            width: collapsed ? 73 : "50%",
             flexShrink: 0,
             [`& .MuiDrawer-paper`]: {
-              width: collapsed ? 73 : "100%",
+              width: collapsed ? 73 : "50%",
               boxSizing: "border-box",
             },
           }}
